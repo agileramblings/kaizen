d891ad7244d9c086ab7d52e8cb72fdbf5be50393
Dave White <dmhwhite@gmail.com>
2018-06-20 14:03:02 -0600

    Update Dislike Items functionality

--- domain.retrospective.tests/DislikeTests.cs
+++ domain.retrospective.tests/DislikeTests.cs
@@ -42,5 +42,61 @@ namespace kaizen.domain.retrospective.tests
             // assert
             Assert.Empty(sut.Dislikes);
         }
+
+        [Fact]
+        public void AnInvitedParticipantCanUpdateTheirOwnDislike()
+        {
+            // arrange
+            const string initialDescription = "Initial description";
+            const string updateDescription = "Update to a description for my Dislike";
+            var sut = GetDefaultRetrospectiveSut();
+            sut.InviteAParticipant(_participants[0]);
+            sut.AddDislikeItem(initialDescription, _participants[0]);
+            Guid dislikeIdentifier = sut.Dislikes.First().Id;
+
+            // act
+            sut.UpdateDislikeItem(dislikeIdentifier, updateDescription, _participants[0]);
+
+            // assert
+            Assert.Equal(updateDescription, sut.Dislikes.First(dl => dl.Id == dislikeIdentifier).Description);
+        }
+
+        [Fact]
+        public void AnInvitedParticipantCannotUpdateSomeoneElsesDislike()
+        {
+            // arrange
+            const string initialDescription = "Initial description";
+            const string updateDescription = "Update to a description for my Dislike";
+            var sut = GetDefaultRetrospectiveSut();
+            sut.InviteAParticipant(_participants[0]);
+            sut.InviteAParticipant(_participants[1]);
+
+            sut.AddDislikeItem(initialDescription, _participants[0]);
+            Guid dislikeIdentifier = sut.Dislikes.First().Id;
+
+            // act & assert
+            Assert.Throws<RetrospectiveItemNotOwnedByParticipantException>(() => sut.UpdateDislikeItem(dislikeIdentifier, updateDescription, _participants[1]));
+
+            // Assert
+            Assert.Equal(initialDescription, sut.Dislikes.First(dl => dl.Id == dislikeIdentifier).Description);
+        }
+
+        [Fact]
+        public void AnInvitedParticipantCannotUpdateADislikeThatDoesNotExist()
+        {
+            // arrange
+            const string initialDescription = "Initial description";
+            const string updateDescription = "Update to a description for my Dislike";
+            var sut = GetDefaultRetrospectiveSut();
+            sut.InviteAParticipant(_participants[0]);
+            sut.AddDislikeItem(initialDescription, _participants[0]);
+            Guid dislikeIdentifier = sut.Dislikes.First().Id;
+
+            // act & assert
+            Assert.Throws<RetrospectiveItemDoesNotExistException>(() => sut.UpdateDislikeItem(Guid.Empty, updateDescription, _participants[0]));
+
+            // Assert
+            Assert.Equal(initialDescription, sut.Dislikes.First(dl => dl.Id == dislikeIdentifier).Description);
+        }
     }
 }
---
+++ domain.retrospective/Events/DislikeUpdated.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using kaizen.domain.@base;
+
+namespace kaizen.domain.retrospective.events
+{
+    public class DislikeUpdated : Event
+    {
+        public string Description;
+        public Guid DislikeIdentifier;
+
+        public DislikeUpdated(Guid dislikeIdentifier, string description)
+        {
+            Description = description;
+            DislikeIdentifier = dislikeIdentifier;
+        }
+    }
+}

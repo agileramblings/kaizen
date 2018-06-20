c7bdf97a6174a63e666aa69f359c7946befd7b4d
Dave White <dmhwhite@gmail.com>
2018-06-20 14:02:39 -0600

    Add UpdateActionItem functions

--- domain.retrospective.tests/ActionItemTests.cs
+++ domain.retrospective.tests/ActionItemTests.cs
@@ -59,5 +59,66 @@ namespace kaizen.domain.retrospective.tests
             // Assert
             Assert.Equal(RetrospectiveState.CollectingSuggestions, sut.State);
         }
+
+        [Fact]
+        public void AnInvitedParticipantCanUpdateTheirOwnAnActionItem()
+        {
+            // arrange
+            const string actionItemDescription = "initial description";
+            const string updateDescription = "Update to a description for my action item";
+            var sut = GetDefaultRetrospectiveSut();
+            sut.InviteAParticipant(_participants[0]);
+            sut.StartCollectingActionItems(OwnerName);
+
+            sut.AddActionItem(actionItemDescription, _participants[0]);
+            Guid actionItemIdentifier = sut.ActionItems.First().Id;
+
+            // act
+            sut.UpdateActionItem(actionItemIdentifier, updateDescription, _participants[0]);
+
+            // assert
+            Assert.Equal(updateDescription, sut.ActionItems.First(ai => ai.Id == actionItemIdentifier).Description);
+        }
+
+        [Fact]
+        public void AnInvitedParticipantCannotUpdateSomeoneElsesAnActionItem()
+        {
+            // arrange
+            const string initialDescription = "Initial description";
+            const string updateDescription = "Update to a description for my action item";
+            var sut = GetDefaultRetrospectiveSut();
+            sut.InviteAParticipant(_participants[0]);
+            sut.InviteAParticipant(_participants[1]);
+            sut.StartCollectingActionItems(OwnerName);
+
+            sut.AddActionItem(initialDescription, _participants[0]);
+            Guid actionItemIdentifier = sut.ActionItems.First().Id;
+
+            // act & assert
+            Assert.Throws<RetrospectiveItemNotOwnedByParticipantException>(() => sut.UpdateActionItem(actionItemIdentifier, updateDescription, _participants[1]));
+
+            // Assert
+            Assert.Equal(initialDescription, sut.ActionItems.First(ai => ai.Id == actionItemIdentifier).Description);
+        }
+
+        [Fact]
+        public void AnInvitedParticipantCannotUpdateAnActionItemThatDoesNotExist()
+        {
+            // arrange
+            const string initialDescription = "Initial description";
+            const string updateDescription = "Update to a description for my action item";
+            var sut = GetDefaultRetrospectiveSut();
+            sut.InviteAParticipant(_participants[0]);
+            sut.StartCollectingActionItems(OwnerName);
+
+            sut.AddActionItem(initialDescription, _participants[0]);
+            Guid actionItemIdentifier = sut.ActionItems.First().Id;
+
+            // act & assert
+            Assert.Throws<RetrospectiveItemDoesNotExistException>(() => sut.UpdateActionItem(Guid.Empty, updateDescription, _participants[0]));
+
+            // Assert
+            Assert.Equal(initialDescription, sut.ActionItems.First(ai => ai.Id == actionItemIdentifier).Description);
+        }
     }
 }
---
+++ domain.retrospective/Events/ActionItemUpdated.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using kaizen.domain.@base;
+
+namespace kaizen.domain.retrospective.events
+{
+    public class ActionItemUpdated : Event
+    {
+        public Guid ActionItemIdentifier;
+        public string Description;
+
+        public ActionItemUpdated(Guid actionItemIdentifier, string description)
+        {
+            ActionItemIdentifier = actionItemIdentifier;
+            Description = description;
+        }
+    }
+}

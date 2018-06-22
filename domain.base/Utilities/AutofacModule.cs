913240583c263b6b76055f5e8357e635e2b7b0be
Dave White <dmhwhite@gmail.com>
2018-06-22 08:44:49 -0600

    Made base.autofacmodule an example class

--- domain.base/Utilities/AutofacModule.cs
+++ domain.base/Utilities/AutofacModule.cs
@@ -6,11 +6,19 @@ using Module = Autofac.Module;
 namespace kaizen.domain.@base.utilities
 {
     // This is used to find all CommandHandlers and EventHandlers dynamically
-    public class AutofacModule : Module
+    public class AutofacExampleModule : Module
     {
         protected override void Load(ContainerBuilder builder)
         {
+            // Load multiple assemblies to scan for handlers
+            var assembliesToScan = new[]
+            {
+                typeof(Command).GetTypeInfo().Assembly
+                // add more assemblies as they are included in the service
+            };
+            // -- or single assembly --
             var asm = Assembly.GetExecutingAssembly();
+
             var handlerType = typeof(ICommandHandler<>);
             builder.RegisterAssemblyTypes(asm).AsClosedTypesOf(handlerType).SingleInstance().PropertiesAutowired();
             var handlerType2 = typeof(IHandles<>);
